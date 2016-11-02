0055857220057703783a02ca85873489a819e814
Dayton Nolan <dayton.nolan@stackwave.com>
2016-11-02 11:07:30 -0500

    Initial commit

---
+++ Scripts/aliases.ps1
@@ -0,0 +1,18 @@
+function d {
+  Set-Location -Path C:\Users\dayton\Development
+}
+
+function compass {
+  Set-Location -Path C:\Users\dayton\Development\compass
+}
+
+function avitide {
+  Set-Location -Path C:\Users\dayton\Development\avitide
+}
+
+function sjs {
+  Set-Location -Path C:\Users\dayton\Development\stackwavejs
+}
+
+Set-Alias lst Get-ChildItem
+Set-Alias c Clear-Host
