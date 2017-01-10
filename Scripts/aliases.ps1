cb3eb637aed67d7efd8c3724b99c93f74cd78a75
Dayton Nolan <daytonn@gmail.com>
2017-01-10 16:19:41 -0600

    Update aliases.ps1

--- Scripts/aliases.ps1
+++ Scripts/aliases.ps1
@@ -1,17 +1,81 @@
 function d {
-  Set-Location -Path C:\Users\dayton\Development
+  Set-Location -Path C:\Users\daytonn\Development
 }
 
 function compass {
-  Set-Location -Path C:\Users\dayton\Development\compass
+  Set-Location -Path C:\Users\daytonn\Development\compass
 }
 
 function avitide {
-  Set-Location -Path C:\Users\dayton\Development\avitide
+  Set-Location -Path C:\Users\daytonn\Development\avitide
 }
 
 function sjs {
-  Set-Location -Path C:\Users\dayton\Development\stackwavejs
+  Set-Location -Path C:\Users\daytonn\Development\stackwavejs
+}
+
+function home {
+  Set-Location -Path C:\Users\daytonn
+}
+
+function st {
+  hg status
+}
+
+function pull {
+  hg pull -u
+}
+
+function push {
+  hg push --new-branch
+}
+
+function stash {
+  hg shelve
+}
+
+function unstash {
+  hg unshelve
+}
+
+function add {
+  hg add
+}
+
+function reload {
+  Invoke-Expression $profile
+}
+
+function vs {
+  Invoke-Item ./*.sln
+}
+
+function s {
+  subl .
+}
+
+function branches {
+  hg branches
+}
+
+function shelve {
+  hg shelve
+}
+
+function unshelve {
+  hg unshelve
+}
+
+function diff {
+  hg diff
+}
+
+function .. {
+  cd ..
+}
+
+function ... {
+  cd ../..
 }
 
 Set-Alias lst Get-ChildItem
